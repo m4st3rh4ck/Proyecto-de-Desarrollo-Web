fab7097f4c64814d0493b9d9b717091d79981063
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-05-21 21:01:38 -0600

    Delete DesarrolloWeb directory

--- DesarrolloWeb/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Establecer la conexión a la base de datos
-$conexion = mysqli_connect('localhost', 'root', '', 'formulario');
-
-// Verificar la conexión
-if (!$conexion) {
-    die("Error de conexión a la base de datos: " . mysqli_connect_error());
-}
-?>
